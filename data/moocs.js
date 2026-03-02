3da4ee5b2c58a668cd29a2e7641e3c72b482a6b1
sudhanshu2025sharma-max <sudhanshu2025sharma@gmail.com>
2026-03-02 14:29:21 +0000

    adding moocs tab

---
+++ data/moocs.js
@@ -0,0 +1,65 @@
+/**
+ * ============================================
+ * MOOCs DATA - WITH EMBEDDED VIDEO SUPPORT
+ * ============================================
+ * To add a new MOOC, copy the template
+ * and paste at the TOP of the array.
+ *
+ * TEMPLATE:
+ * {
+ *     title: "MOOC Title",
+ *     authors: "Instructor Name(s)",
+ *     organization: "Platform / Institution",
+ *     year: "2024-2025",
+ *     duration: "8 Weeks",
+ *     platform: "NPTEL / Coursera / edX",     // Optional
+ *     certificate: "Yes / No",                 // Optional
+ *     videoLink: "https://youtube.com/...",   // YouTube/Vimeo URL for embedded player
+ *     courseLink: "https://course-url.com"    // Optional - Link to full course page
+ * },
+ *
+ * SUPPORTED VIDEO PLATFORMS:
+ * - YouTube (youtube.com, youtu.be)
+ * - Other URLs will show a "Watch Video" placeholder
+ *
+ * ============================================
+ */
+
+var MOOCS_DATA = [
+    {
+        title: "Science Communication: Research Productivity and Data Analytics using Open Source Software (2026)",
+        authors: "By Prof. Nabi Hasan, Prof. Mohit Garg",
+        organization: "IIT Delhi",
+        year: "2026",
+        duration: "12 Weeks",
+        platform: "Swayam",
+        certificate: "Yes",
+        videoLink: "https://www.youtube.com/watch?v=Fu8tB7wQyLc",
+        courseLink: "https://onlinecourses.nptel.ac.in/noc26_hs22/preview"
+    },
+    
+    {
+        title: "Science Communication: Research Productivity and Data Analytics using Open Source Software (2025)",
+        authors: "By Prof. Nabi Hasan, Prof. Mohit Garg",
+        organization: "IIT Delhi",
+        year: "2025",
+        duration: "12 Weeks",
+        platform: "Swayam",
+        certificate: "Yes",
+        videoLink: "https://www.youtube.com/watch?v=Fu8tB7wQyLc&t=1s",
+        courseLink: "https://onlinecourses.nptel.ac.in/noc25_hs70/preview"
+    },
+    {
+        title: "Science Communication: Research Productivity and Data Analytics using Open Source Software (2024)",
+        authors: "By Prof. Nabi Hasan, Prof. Mohit Garg",
+        organization: "IIT Delhi",
+        year: "2024",
+        duration: "14 Weeks",
+        platform: "Swayam",
+        certificate: "Yes",
+        videoLink: "https://www.youtube.com/watch?v=Fu8tB7wQyLc",
+        courseLink: "https://onlinecourses.nptel.ac.in/noc24_hs01/preview"
+    },
+
+
+];
