8243ea50066bdf81199908f0d46decb299dcb242
sudhanshu2025sharma-max <sudhanshu2025sharma@gmail.com>
2026-04-05 14:54:48 +0000

    adding Peoples Tab_adding Nikhil image

--- data/people.js
+++ data/people.js
@@ -25,7 +25,7 @@ var PEOPLE_DATA = {
     projectstaff: [
         {
             name: "Mr. Nikhil Raj",
-            image: "ravi_verma.jpg",
+            image: "images/people/Nikhil.jpg",
             duration: "June 2025 - Present",
             currentDesignation: "Research Assistant(ICSSR)"
         }
